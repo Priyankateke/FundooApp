d362180f6f0f7fa2a7807e4ab89acb9c390ce7ec
Priyankateke <priiyankateke1705@gmail.com>
2020-06-26 18:44:01 +0530

    ADDED: Refresh Button On Appbar

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,9 +1,10 @@
+// Imports
 import React from 'react'
 import PropTypes from 'prop-types';
 import classNames from 'classnames'
 import keep from '../../assets/keep_logo.png'
-import { fade} from '@material-ui/core/styles';
-import { withStyles, AppBar,Drawer, ListItemIcon,List,ListItem,ListItemText, CssBaseline} from '@material-ui/core'
+import { fade } from '@material-ui/core/styles';
+import { withStyles, AppBar, Drawer, ListItemIcon, List, ListItem, ListItemText, CssBaseline, Tooltip } from '@material-ui/core'
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -19,12 +20,16 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Divider from '@material-ui/core/Divider';
 import Delete from '@material-ui/icons/Delete';
-import Alarm  from '@material-ui/icons/Alarm';
+import Alarm from '@material-ui/icons/Alarm';
 import Notes from '@material-ui/icons/Notes';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Grid from '@material-ui/icons/Apps'
+import ListIcon from '@material-ui/icons/List'
+
 
 const drawerWidth = 240;
 
+// Styles
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -63,99 +68,100 @@ const styles = theme => ({
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing.unit * 2,
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing.unit * 3,
-        width: 'auto',
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto',
     },
-    },
-    avatar: {
-        margin: 10,
-    },
-    searchIcon: {
-        width: theme.spacing.unit * 9,
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing.unit * 3,
-    },
+  },
+  avatar: {
+    margin: 10,
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
 
-    grow: {
-        flexGrow: 1,
-    },
+  grow: {
+    flexGrow: 1,
+  },
 
-    title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
     },
+  },
 
-    inputRoot: {
-        color: 'inherit',
-        width: '100%',
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '40ch',
     },
-    inputInput: {
-        paddingTop: theme.spacing.unit,
-        paddingRight: theme.spacing.unit,
-        paddingBottom: theme.spacing.unit,
-        paddingLeft: theme.spacing.unit * 10,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '40ch',
-        },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
     },
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing.unit * 7 + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9 + 1,
     },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing.unit * 7 + 1,
-        [theme.breakpoints.up('sm')]: {
-        width: theme.spacing.unit * 9 + 1,
-        },
-    },
+  },
 });
 
-class Dashboard extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            anchorEl: null,
-            open:false,
-        };
-    }
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      anchorEl: null,
+      open: false,
+    };
+  }
 
+  // Events
   handleDrawerOpen = () => {
     this.setState({ open: true });
   };
@@ -166,43 +172,49 @@ class Dashboard extends React.Component{
 
   handleProfileMenuOpen = event => {
     this.setState({ anchorEl: event.currentTarget });
-};
+  };
 
-changeView = (event, viewType)=>{
-  event.preventDefault()
-  this.setState({typeOfNote:viewType})
-}
-
-logOut(event) {
+  changeView = (event, viewType) => {
     event.preventDefault()
-}
-handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-};
+    this.setState({ typeOfNote: viewType })
+  }
 
+  logOut(event) {
+    event.preventDefault()
+  }
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleViewClick = () => {
+    this.setState({ view: !this.state.view });
+  }
+
+  // render
   render() {
+    // Set Properties
     const { classes, theme } = this.props;
     const { anchorEl } = this.state;
     const isMenuOpen = Boolean(anchorEl);
 
     const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMenuOpen}
-            onClose={this.handleMenuClose}
-        >
-            <MenuItem onClick={this.logOut.bind(this)}>Logout</MenuItem>
-        </Menu>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem onClick={this.logOut.bind(this)}>Logout</MenuItem>
+      </Menu>
     );
     return (
       <div className={classes.root}>
         <CssBaseline />
         <AppBar
           position="fixed"
-          className={classNames(classes.appBar, {[classes.appBarShift]: this.state.open,})}>
-        <Toolbar disableGutters={!this.state.open}>
+          className={classNames(classes.appBar, { [classes.appBarShift]: this.state.open, })}>
+          <Toolbar disableGutters={!this.state.open}>
             <IconButton
               color="inherit"
               aria-label="Open drawer"
@@ -211,36 +223,48 @@ handleMenuClose = () => {
                 [classes.hide]: this.state.open,
               })}
             >
-            <MenuIcon />
+              <MenuIcon />
             </IconButton>
-            <img className={classes.logo} src={keep} alt="Logo" /> 
+            <img className={classes.logo} src={keep} alt="Logo" />
             <Typography variant="h6" color="inherit" noWrap>
-                FundooNote
+              FundooNote
             </Typography>
             <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                    <SearchIcon />
-                    </div>
-                        <InputBase placeholder="Search…" classes={{root: classes.inputRoot,input: classes.inputInput,}}/>
-                    </div>
-                    <div className={classes.grow} />
-                        <div className={classes.sectionDesktop}>
-                            <IconButton color="inherit">
-                                <Refresh/>
-                            </IconButton>
-                            <IconButton color="inherit">
-                                <NotificationsIcon/>
-                            </IconButton>
-                           <IconButton
-                                edge="end"
-                                aria-label="account user"
-                                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                                onClick={this.handleProfileMenuOpen}
-                                color="inherit">
-                                <AccountCircle />
-                            </IconButton>
-                        </div>
-            </Toolbar>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput, }} />
+            </div>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              <IconButton color="inherit">
+                <Refresh />
+              </IconButton>
+
+              <div>
+                <Tooltip title={this.state.view ? "Grid View" : "List View"}>
+                  <IconButton
+                    color="inherit"
+                    onClick={this.handleViewClick}
+                    aria-label="List/Grid"
+                  >
+                    {this.state.view ? <Grid /> : <ListIcon />}
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <IconButton color="inherit">
+                <NotificationsIcon />
+              </IconButton>
+              <IconButton
+                edge="end"
+                aria-label="account user"
+                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                onClick={this.handleProfileMenuOpen}
+                color="inherit">
+                <AccountCircle />
+              </IconButton>
+            </div>
+          </Toolbar>
         </AppBar>
         {renderMenu}
         <Drawer
@@ -263,25 +287,25 @@ handleMenuClose = () => {
           </div>
           <Divider />
           <List>
-          <ListItem button key='Notes' onClick={(event)=>{this.changeView(event, 'Keep')}}>
-            <ListItemIcon><Notes/></ListItemIcon>
-            <ListItemText>Notes</ListItemText>
+            <ListItem button key='Notes' onClick={(event) => { this.changeView(event, 'Keep') }}>
+              <ListItemIcon><Notes /></ListItemIcon>
+              <ListItemText>Notes</ListItemText>
             </ListItem>
             <ListItem button key='Reminder'>
-            <ListItemIcon><Alarm/></ListItemIcon>
-            <ListItemText>Reminder</ListItemText>
+              <ListItemIcon><Alarm /></ListItemIcon>
+              <ListItemText>Reminder</ListItemText>
             </ListItem>
           </List>
           <Divider />
           <List>
 
-        <ListItem button key='Archive' onClick={(event)=>{this.changeView(event, 'Archive')}}>
-            <ListItemIcon><InboxIcon/></ListItemIcon>
-            <ListItemText>Archive</ListItemText>  
+            <ListItem button key='Archive' onClick={(event) => { this.changeView(event, 'Archive') }}>
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText>Archive</ListItemText>
             </ListItem>
-        <ListItem button key='Delete' onClick={(event)=>{this.changeView(event, 'Trash')}}>
-            <ListItemIcon><Delete/></ListItemIcon>
-            <ListItemText>Trash</ListItemText>
+            <ListItem button key='Delete' onClick={(event) => { this.changeView(event, 'Trash') }}>
+              <ListItemIcon><Delete /></ListItemIcon>
+              <ListItemText>Trash</ListItemText>
             </ListItem>
           </List>
         </Drawer>
@@ -294,6 +318,5 @@ Dashboard.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired,
 };
-
 
 export default ((withStyles(styles, { withTheme: true })(Dashboard))) 
