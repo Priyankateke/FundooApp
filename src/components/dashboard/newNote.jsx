2e5883807160f2eac39285bc1f4544044604e5d9
Priyankateke <priiyankateke1705@gmail.com>
2020-06-28 16:09:58 +0530

    ADDED: Icons And Close Boutton In Create Note Card

--- src/components/dashboard/newNote.jsx
+++ src/components/dashboard/newNote.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import '../../styles/createNote.css'
 import CheckBoxOutlinedIcon from '@material-ui/icons/CheckBoxOutlined';
 import BrushIcon from '@material-ui/icons/Brush';
@@ -13,8 +14,18 @@ import UndoOutlinedIcon from '@material-ui/icons/UndoOutlined';
 import RedoOutlinedIcon from '@material-ui/icons/RedoOutlined';
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import Container from "@material-ui/core/Container";
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import { withStyles } from '@material-ui/core'
+import Button from '@material-ui/core/Button';
 
-export default class NewNote extends Component {
+const styles = theme => ({
+    input: {
+        outlined: 'none',
+    },
+})
+
+class NewNote extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -23,51 +34,66 @@ export default class NewNote extends Component {
     }
     onHandleClickaway = () => {
         this.setState({
-          collapse: !this.state.collapse,
+            collapse: !this.state.collapse,
         });
-      };
-      handleClick = () => {
+    };
+    handleClick = () => {
         this.setState({
-          collapse: false,
+            collapse: false,
         });
-      };
+    };
     render() {
+        const { classes, theme } = this.props;
+
         return (
-             <Container>
+            <Container>
                 <ClickAwayListener onClickAway={this.onHandleClickaway}>
-                 <div>
-                    {
-                        this.state.collapse ?
-                            <div className="defaultContainer"
-                                onClick={this.handleClick}>
-                                <div className="font">Take a note...</div>
-                                <div className="icons"><CheckBoxOutlinedIcon /></div>
-                                <div className="icons"><BrushIcon /></div>
-                                <div className="icons"><ImageOutlinedIcon /></div>
-                            </div> :
-                            <div className="expandedContainer"
-                                onClick={this.handleClick}>
-                                <div className="summary">
-                                    <div className="title">Title</div>
-                                    <div><FiberPinOutlinedIcon /></div>
+                    <div>
+                        {
+                            this.state.collapse ?
+                                <div className="defaultContainer"
+                                    onClick={this.handleClick}>
+                                    <div className="font">Take a note...</div>
+                                    <div className="icons"><CheckBoxOutlinedIcon /></div>
+                                    <div className="icons"><BrushIcon /></div>
+                                    <div className="icons"><ImageOutlinedIcon /></div>
+                                </div> :
+                                <div className="expandedContainer"
+                                    onClick={this.handleClick}>
+                                    <div className="summary">
+                                        <div>
+                                            <TextField className={classes.input} placeholder="Title" inputProps={{ 'aria-label': 'description' }} />
+                                        </div>
+                                        {/* <div className="title">Title</div> */}
+                                        <div><FiberPinOutlinedIcon /></div>
+                                    </div>
+                                    <div>
+                                        <TextField className={classes.input} placeholder="Take a note.." inputProps={{ 'aria-label': 'description' }} />
+                                    </div>
+                                    {/* <div className="title">Take a note..</div> */}
+                                    <div className="noteIcons">
+                                        <AddAlertOutlinedIcon fontSize="small" />
+                                        <PersonAddOutlinedIcon fontSize="small" />
+                                        <ColorLensOutlinedIcon fontSize="small" />
+                                        <ImageOutlinedIcon fontSize="small" />
+                                        <ArchiveOutlinedIcon fontSize="small" />
+                                        <MoreVertOutlinedIcon fontSize="small" />
+                                        <UndoOutlinedIcon fontSize="small" />
+                                        <RedoOutlinedIcon fontSize="small" />
+                                        <Button size="small">Close</Button>
+                                    </div>
                                 </div>
-                                <div className="title">Take a note..</div>
-                                <div className="noteIcons">
-                                    <AddAlertOutlinedIcon fontSize="small" />
-                                    <PersonAddOutlinedIcon fontSize="small" />
-                                    <ColorLensOutlinedIcon fontSize="small" />
-                                    <ImageOutlinedIcon fontSize="small" />
-                                    <ArchiveOutlinedIcon fontSize="small" />
-                                    <MoreVertOutlinedIcon fontSize="small" />
-                                    <UndoOutlinedIcon fontSize="small" />
-                                    <RedoOutlinedIcon fontSize="small" />
-                                </div>
-                            </div>
-                    }
-             </div>
-            </ClickAwayListener>
-       </Container >
+                        }
+                    </div>
+                </ClickAwayListener>
+            </Container >
         );
     }
 }
 
+NewNote.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+export default ((withStyles(styles, { withTheme: true })(NewNote))) 
